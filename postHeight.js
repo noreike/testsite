ba8cefb14235bbbbb58accaad091a28f187119c5
noreike <32568183+noreike@users.noreply.github.com>
2021-05-25 16:10:18 +0900

    Update postHeight.js

--- postHeight.js
+++ postHeight.js
@@ -2,6 +2,6 @@ $(window).on('load resize', function(){
     setTimeout(function(){
         var height = $('body').height();
         window.parent.postMessage(height, 'https://www.asahi-life.co.jp/')
-        window.parent.postMessage(height, window.parent.origin);
+        //window.parent.postMessage(height, window.parent.origin);
     },500);
 });
