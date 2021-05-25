4c3468ef586bf149b7fbaddb8722814ef30b1550
noreike <32568183+noreike@users.noreply.github.com>
2021-05-25 16:03:31 +0900

    Add files via upload

---
+++ postHeight.js
@@ -0,0 +1,7 @@
+$(window).on('load resize', function(){
+    setTimeout(function(){
+        var height = $('body').height();
+        //window.parent.postMessage(height, 'https://www.asahi-life.co.jp/')
+        window.parent.postMessage(height, window.parent.origin);
+    },500);
+});
