361c4359277af832d43f72047539810d95df61e7
noreike <32568183+noreike@users.noreply.github.com>
2021-05-25 16:41:46 +0900

    Update postHeight.js

--- postHeight.js
+++ postHeight.js
@@ -1,7 +1,7 @@
 $(window).on('load resize', function(){
     setTimeout(function(){
         var height = $('body').height();
-        window.parent.postMessage(height, 'https://asahi.cms-wr.biglobe.ne.jp/');
+        window.parent.postMessage(height, 'http://dev2.ball-inc.co.jp/');
         //window.parent.postMessage(height, window.parent.origin);
     },500);
 });
