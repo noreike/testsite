b3ba37f0bea0051da11313159a4e75040c8fc574
noreike <32568183+noreike@users.noreply.github.com>
2021-08-02 11:43:18 +0900

    change domain name

--- postHeight.js
+++ postHeight.js
@@ -6,6 +6,6 @@ $(window).on('load resize', function(){
     //window.parent.postMessage(msg, 'http://dev2.ball-inc.co.jp/');
     setTimeout(function(){
         msg.height = $('body').height();
-        window.parent.postMessage(msg, 'http://dev2.ball-inc.co.jp/');
+        window.parent.postMessage(msg, 'https://asahi.cms-wr.biglobe.ne.jp/');
     },500);
 });
